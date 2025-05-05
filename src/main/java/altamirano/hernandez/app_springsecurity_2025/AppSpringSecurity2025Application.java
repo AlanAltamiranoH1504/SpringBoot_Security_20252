5f336153bc0fee2cd195967251515ec87d8a8402
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-05-05 09:43:04 -0600

    initial commit

---
+++ src/main/java/altamirano/hernandez/app_springsecurity_2025/AppSpringSecurity2025Application.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.app_springsecurity_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringSecurity2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpringSecurity2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/altamirano/hernandez/app_springsecurity_2025/AppSpringSecurity2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.app_springsecurity_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringSecurity2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
